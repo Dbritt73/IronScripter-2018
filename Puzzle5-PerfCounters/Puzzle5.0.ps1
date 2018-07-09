9ed24e7901563b3b5a7b73a0149f65e889bab171
Darrin Britton <brittod@wwu.edu>
2018-07-09 07:48:30 -0700

    Fixed some formatting in puzzle 5

--- Puzzle5-PerfCounters/Puzzle5.0.ps1
+++ Puzzle5-PerfCounters/Puzzle5.0.ps1
@@ -1,6 +1,7 @@
 Function Get-PerfCounters {
     [CmdletBinding()]
     Param (
+
         [Parameter( ValuefromPipeline=$true, 
                     ValueFromPipelineByPropertyName=$true)]
         [String[]]$ComputerName,
@@ -8,56 +9,70 @@ Function Get-PerfCounters {
         [switch]$HTML,
 
         [switch]$csv
+
     )
 
     Begin {}
 
     Process {
+
         Foreach ($computer in $ComputerName) {
+
             Try {
 
                 $CIM = @{
+
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'Win32_PerfFormattedData_PerfOS_Processor';
                     'ErrorAction' = 'Stop'
+
                 }
 
                 $Processor = Get-CimInstance @CIM
 
                 $CIM = @{
+
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'CIM_LogicalDisk';
                     'ErrorAction' = 'Stop'
+
                 }
+
                 $Disk = Get-CimInstance @CIM | Where-Object {$_.Name -eq 'C:'}
 
                 $CIM = @{
+
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'Win32_PerfFormattedData_PerfOS_Memory';
                     'ErrorAction' = 'Stop'
+
                 }
 
                 $Memory =  Get-CimInstance @CIM
 
                 $CIM = @{
+
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'Win32_PerfFormattedData_Tcpip_NetworkAdapter';
                     'ErrorAction' = 'Stop'
+
                 }
 
                 $Network = Get-CimInstance @CIM | Where-Object {$_.BytesTotalPerSec -ne 0}
 
                 $PerfProps = @{
+
                     'ComputerName' = $computer;
                     'TimeStamp' = (Get-Date);
                     'PercentageProcessorTime' = [int]($Processor | Measure-Object PercentProcessorTime -Average | Select-Object -ExpandProperty Average);
                     'PercentFreeSpace(C:)' = [int](($disk.FreeSpace / $disk.Size) * 100);
                     'MemoryCommitedBytes' = $Memory.PercentCommittedBytesInUse;
                     'TotalNetBytesSent' = $Network | Sort-Object BytesTotalPerSec -Descending | Select-Object Name, BytesTotalPerSec -First 2
+
                 }
 
                 $perfobj = New-Object -TypeName psobject -Property $perfProps
@@ -65,15 +80,20 @@ Function Get-PerfCounters {
                 Write-Output $perfobj 
 
             } Catch {}
+
         }
+
     }
 
     End {}
+
 }
-#***********************************************************************************************************************
+
+
 Function Invoke-Output {
     [cmdletBinding()]
     Param (
+
         [Parameter( ValuefromPipeline=$true, 
         ValueFromPipelineByPropertyName=$true)]
         [String[]]$ComputerName,
@@ -81,31 +101,37 @@ Function Invoke-Output {
         [Switch]$Html,
 
         [Switch]$csv
+
     )
 
     Begin {}
 
     Process {
-       # foreach ($computer in $computername) {
-            Try {
-                if ($PSBoundParameters.ContainsKey('html')) {
 
-                    $Style = "<style>$(Get-content .\style.css)</style>"
-                    $HTMLReport = $ComputerName | Get-PerfCounters | ConvertTo-Html -As 'Table' -Fragment -PreContent "<h2>PC Perf Counters</h2>" | Out-String
+        Try {
 
-                    $HTMLParams = @{
-                        'Head' = "<title>PC Performance Counters</title>$Style";
-                        'PreContent' = "<h1>PC Performance</h1>";
-                        'PostContent' = "$HTMLReport"
-                    }
+            if ($PSBoundParameters.ContainsKey('html')) {
 
-                    ConvertTo-Html @HTMLParams | Out-file .\PCPerfCounters.html
+                $Style = "<style>$(Get-content .\style.css)</style>"
+                $HTMLReport = $ComputerName | Get-PerfCounters | ConvertTo-Html -As 'Table' -Fragment -PreContent "<h2>PC Perf Counters</h2>" | Out-String
+
+                $HTMLParams = @{
+
+                    'Head' = "<title>PC Performance Counters</title>$Style";
+                    'PreContent' = "<h1>PC Performance</h1>";
+                    'PostContent' = "$HTMLReport"
+
                 }
-            } catch {}
 
-       # }
+                ConvertTo-Html @HTMLParams | Out-file .\PCPerfCounters.html
+
+            }
+
+        } catch {}
+
 
     }
 
     End {}
+    
 }
