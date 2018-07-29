a87a8d9f36b2a23c556356c910546717b3df4714
Darrin Britton <brittod@wwu.edu>
2018-07-29 15:54:47 -0700

    Added puzzle 7, and edits to puzzle 5

---
+++ Puzzle 7 - Custom Class/Puzzle7-IronComputerInformation.ps1
@@ -0,0 +1,140 @@
+Class IronComputerInformation {
+
+    [string]$ComputerName
+    [string]$BIOSmfgr
+    [string]$BIOSversion
+    [String]$Domain
+    [int]$NumberofProcessors
+    [int]$NumberofCores
+    [double]$TotalPhyscialMemory
+    [string]$OSName
+    [string]$OSArch
+    [string]$TimeZone
+    [double]$SizeofCDrive
+    [double]$FreeSpace
+    
+    #Default Constructor
+    IronComputerInformation(){}
+
+    #Contructor with arguments
+    IronComputerInformation ([String]$ComputerName, [string]$Domain) {
+
+        #try-catch block had broke this
+        if (($ComputerName -eq '') -or ($ComputerName -eq $Null)) {
+
+            #Write-output "ComputerName is empty or null"
+            throw [System.InvalidOperationException]::new("ComputerName is empty or Null")
+
+        } 
+
+        if (($Domain -eq '') -or ($Domain -eq $Null)) {
+
+            #Write-Output "Domain is empty or Null"
+            throw [System.InvalidOperationException]::new("Domain is empty or Null")
+
+        }
+
+            $this.ComputerName = $ComputerName
+            $this.Domain = $Domain
+
+        $Error[0]
+        
+    }
+
+    [void] GetComputerInfo() {
+
+        $compsys = Get-CimInstance -ClassName 'Win32_ComputerSystem'
+
+        $this.ComputerName = $compsys.Name
+        $this.TotalPhyscialMemory = [math]::Ceiling($compsys.TotalPhysicalMemory / 1GB)
+
+        $this.Domain = $compsys.Domain
+        $this.NumberofProcessors = $compsys.NumberofProcessors
+
+    }
+    
+    [void] GetCoresInfo() {
+
+        $proc = Get-CimInstance -ClassName 'Win32_Processor'
+
+        $this.NumberofCores = $proc.NumberOfCores
+
+    }
+
+    [void] GetBiosInfo() {
+
+        $bios = Get-CimInstance -ClassName 'Win32_BIOS'
+
+        $this.BIOSmfgr = $bios.Manufacturer
+        $this.BIOSversion = $bios.Version
+
+    }
+
+    [void] GetOsInfo() {
+
+        $OS = Get-CimInstance -ClassName 'win32_OperatingSystem'
+
+        $this.OSName = $os.Caption
+        $this.OSArch = $os.OSArchitecture
+
+    }
+
+    [void] GetTimeZoneInfo() {
+
+        $this.TimeZone = (Get-TimeZone).DisplayName
+
+    }
+
+    [void] GetDiskInfo() {
+
+        $disks = Get-CimInstance -ClassName 'Win32_LogicalDisk' -Filter "DeviceID='C:'"
+
+        $this.SizeofCDrive = [math]::Round(($disks.Size /1GB) , 2)
+        $this.FreeSpace = [math]::Round(($disks.FreeSpace / 1GB) , 2)
+
+    }
+
+    [void] GetAllInfo() {
+
+        $this.GetComputerInfo()
+        $this.GetCoresInfo()
+        $this.GetBiosInfo()
+        $this.GetOsInfo()
+        $this.GetTimeZoneInfo()
+        $this.GetDiskInfo()
+
+    }
+
+    [double] CalcFreeSpacePerc() {
+
+        if (($this.SizeofCDrive -eq 0) -or ($this.FreeSpace -eq 0)) {
+            
+            $this.GetDiskInfo()
+
+        }
+
+        $perc = ($this.FreeSpace / $this.SizeofCDrive) * 100
+
+        return [math]::Round($perc, 3)
+
+    }
+
+    #Contructor to take local computername and get all info
+    IronComputerInformation ([string]$ComputerName) {
+
+        if (($ComputerName -eq '') -or ($ComputerName -eq $Null)) {
+
+            throw [System.InvalidOperationException]::new("computername is empty or nulll")
+
+        }
+
+        if ($ComputerName -ne $env:COMPUTERNAME) {
+
+            Throw "$ComputerName not equal to local computername"
+            
+        }
+
+        $this.GetAllInfo()
+    }
+
+}
--- Puzzle5-PerfCounters/Puzzle5.0.ps1
+++ Puzzle5-PerfCounters/Puzzle5.0.ps1
@@ -13,64 +13,100 @@ Function Get-PerfCounters {
     Begin {}
 
     Process {
+    
         Foreach ($computer in $ComputerName) {
+        
             Try {
 
                 $CIM = @{
+                
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'Win32_PerfFormattedData_PerfOS_Processor';
                     'ErrorAction' = 'Stop'
+                    
                 }
 
                 $Processor = Get-CimInstance @CIM
 
                 $CIM = @{
+                
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'CIM_LogicalDisk';
                     'ErrorAction' = 'Stop'
+                    
                 }
+                
                 $Disk = Get-CimInstance @CIM | Where-Object {$_.Name -eq 'C:'}
 
                 $CIM = @{
+                
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'Win32_PerfFormattedData_PerfOS_Memory';
                     'ErrorAction' = 'Stop'
+                    
                 }
 
                 $Memory =  Get-CimInstance @CIM
 
                 $CIM = @{
+                
                     'ComputerName' = $computer;
                     'NameSpace' = 'root/CimV2';
                     'ClassName' = 'Win32_PerfFormattedData_Tcpip_NetworkAdapter';
                     'ErrorAction' = 'Stop'
+                    
                 }
 
                 $Network = Get-CimInstance @CIM | Where-Object {$_.BytesTotalPerSec -ne 0}
 
                 $PerfProps = @{
+                
                     'ComputerName' = $computer;
                     'TimeStamp' = (Get-Date);
-                    'PercentageProcessorTime' = [int]($Processor | Measure-Object PercentProcessorTime -Average | Select-Object -ExpandProperty Average);
+                    'PercentageProcessorTime' = [int]($Processor | Measure-Object -Property PercentProcessorTime -Average | Select-Object -ExpandProperty Average);
                     'PercentFreeSpace(C:)' = [int](($disk.FreeSpace / $disk.Size) * 100);
                     'MemoryCommitedBytes' = $Memory.PercentCommittedBytesInUse;
-                    'TotalNetBytesSent' = $Network | Sort-Object BytesTotalPerSec -Descending | Select-Object Name, BytesTotalPerSec -First 2
+                    'TotalNetBytesSent' = $Network | Sort-Object -Property BytesTotalPerSec -Descending | Select-Object -Property Name, BytesTotalPerSec -First 2
+                    
                 }
 
                 $perfobj = New-Object -TypeName psobject -Property $perfProps
                 $perfobj.PSObject.TypeNames.Insert(0,'CompPerf.Object')
-                Write-Output $perfobj 
+                Write-Output -InputObject $perfobj 
 
-            } Catch {}
+            } Catch {
+            
+                # get error record
+                [Management.Automation.ErrorRecord]$e = $_
+
+                # retrieve information about runtime error
+                $info = [PSCustomObject]@{
+                
+                  Exception = $e.Exception.Message
+                  Reason    = $e.CategoryInfo.Reason
+                  Target    = $e.CategoryInfo.TargetName
+                  Script    = $e.InvocationInfo.ScriptName
+                  Line      = $e.InvocationInfo.ScriptLineNumber
+                  Column    = $e.InvocationInfo.OffsetInLine
+                  
+                }
+                
+                # output information. Post-process collected info, and log info (optional)
+                $info
+            }
+            
         }
+        
     }
 
     End {}
+    
 }
-#***********************************************************************************************************************
+
+
 Function Invoke-Output {
     [cmdletBinding()]
     Param (
@@ -88,20 +124,41 @@ Function Invoke-Output {
     Process {
        # foreach ($computer in $computername) {
             Try {
+            
                 if ($PSBoundParameters.ContainsKey('html')) {
 
-                    $Style = "<style>$(Get-content .\ComputerOwnership.css)</style>"
-                    $HTMLReport = $ComputerName | Get-PerfCounters | ConvertTo-Html -As 'Table' -Fragment -PreContent "<h2>PC Perf Counters</h2>" | Out-String
+                    $Style = "<style>$(Get-content -Path .\ComputerOwnership.css)</style>"
+                    $HTMLReport = $ComputerName | Get-PerfCounters | ConvertTo-Html -As 'Table' -Fragment -PreContent '<h2>PC Perf Counters</h2>' | Out-String
 
                     $HTMLParams = @{
+                    
                         'Head' = "<title>PC Performance Counters</title>$Style";
-                        'PreContent' = "<h1>PC Performance</h1>";
+                        'PreContent' = '<h1>PC Performance</h1>';
                         'PostContent' = "$HTMLReport"
+                        
                     }
 
-                    ConvertTo-Html @HTMLParams | Out-file .\PCPerfCounters.html
+                    ConvertTo-Html @HTMLParams | Out-file -FilePath .\PCPerfCounters.html
                 }
-            } catch {}
+            } catch {
+            
+                # get error record
+                [Management.Automation.ErrorRecord]$e = $_
+
+                # retrieve information about runtime error
+                $info = [PSCustomObject]@{
+                
+                  Exception = $e.Exception.Message
+                  Reason    = $e.CategoryInfo.Reason
+                  Target    = $e.CategoryInfo.TargetName
+                  Script    = $e.InvocationInfo.ScriptName
+                  Line      = $e.InvocationInfo.ScriptLineNumber
+                  Column    = $e.InvocationInfo.OffsetInLine
+                }
+                
+                # output information. Post-process collected info, and log info (optional)
+                $info
+            }
 
        # }
 
