53f518301198ffd150133c8b12eb3e935e98c3c8
Darrin Britton <Darrin.Britton@Outlook.com>
2019-04-21 14:08:38 -0700

    remvoe white space

--- Puzzle 2 - Custom Computer Information/PSCustomComputerInfo/public/Get-ComputerInformation.ps1
+++ Puzzle 2 - Custom Computer Information/PSCustomComputerInfo/public/Get-ComputerInformation.ps1
@@ -2,13 +2,13 @@ Function Get-ComputerInformation {
     <#
     .SYNOPSIS
     Retrieves general information about a computer
-    
+
     .DESCRIPTION
     Get-ComputerInformation utilizes the Common Information Model (CIM) to query the win32_operatingsystem instance to
     the OS Name, OS Version, OS Manufacturer, Windows directory path, the language locale, Available Physical Memory, 
     Available Virtual Memory, Total Virtual Memory, and Logical Disk information from the Win32_logicaldisk instance via
     a helper function Get-ComputerDiskInformation.
-    
+
     .EXAMPLE
     Get-ComputerInformation -ComputerName 'SERVER1'
 
@@ -17,7 +17,7 @@ Function Get-ComputerInformation {
 
     .EXAMPLE
     (Get-Content .\computers.txt) | Get-ComputerInformation
-    
+
     .NOTES
     Compatable with PowerShell 6.0
     Requires helper function Get-ComputerDiskInformation
@@ -41,67 +41,64 @@ Function Get-ComputerInformation {
 
                 $WMI = @{
 
-                    'ComputerName' = $Computer;
-                    'Class' = 'Win32_OperatingSystem';
-                    'ErrorAction' = 'Stop'
+                    'ComputerName' = $Computer
+                    'Class'        = 'Win32_OperatingSystem'
+                    'ErrorAction'  = 'Stop'
 
                 }
 
                 $OS = Get-CimInstance @WMI
-                
+
                 #object returned by Get-ComputerDiskInformation has properties
                 #that correspond to properties returned by Get-WmiObject win32_logicaldisk
                 $disks = Get-ComputerDiskInformation -ComputerName $Computer
 
-                $Properties = @{
+                $Properties = [Ordered]@{
 
-                    'ComputerName' = $computer;
-                    'OSName' = $OS.Caption;
-                    #'OSVersion' = "$($OS.ServicePackMajorVersion)" + "." + "$($OS.ServicePackMinorVersion)";
-                    'OSVersion' = $OS.Version;
-                    'OSManufacturer' = $OS.Manufacturer;
-                    'WindowsDir' = $OS.WindowsDirectory;
-                    'Locale' = $OS.MUILanguages;
-                    'AvailPhysicalMemory' = $OS.FreePhysicalMemory;
-                    'TotalVirtualMemory' = $OS.TotalVirtualMemorySize;
-                    'AvailVirtualMemory' = $OS.FreeVirtualMemory;
-                    'LogicalDisks' = $Disks
+                    'ComputerName'        = $computer
+                    'OSName'              = $OS.Caption
+                    #'OSVersion'          = "$($OS.ServicePackMajorVersion)" + "." + "$($OS.ServicePackMinorVersion)"
+                    'OSVersion'           = $OS.Version
+                    'OSManufacturer'      = $OS.Manufacturer
+                    'WindowsDir'          = $OS.WindowsDirectory
+                    'Locale'              = $OS.MUILanguages
+                    'AvailPhysicalMemory' = $OS.FreePhysicalMemory
+                    'TotalVirtualMemory'  = $OS.TotalVirtualMemorySize
+                    'AvailVirtualMemory'  = $OS.FreeVirtualMemory
+                    'LogicalDisks'        = $Disks
 
                 }
 
-                Write-debug -Message 'Test Properties'
-
                 $Object = New-Object -TypeName PSObject -Property $Properties
                 $Object.PSObject.TypeNames.Insert(0,'Custom.ComputerInformation')
-                Write-Debug -Message 'Test output'
                 Write-Output -InputObject $Object
 
             } Catch {
-            
+
                 # get error record
                 [Management.Automation.ErrorRecord]$e = $_
 
                 # retrieve information about runtime error
                 $info = [PSCustomObject]@{
-                
-                  Exception = $e.Exception.Message
-                  Reason    = $e.CategoryInfo.Reason
-                  Target    = $e.CategoryInfo.TargetName
-                  Script    = $e.InvocationInfo.ScriptName
-                  Line      = $e.InvocationInfo.ScriptLineNumber
-                  Column    = $e.InvocationInfo.OffsetInLine
-                  
+
+                    Exception = $e.Exception.Message
+                    Reason    = $e.CategoryInfo.Reason
+                    Target    = $e.CategoryInfo.TargetName
+                    Script    = $e.InvocationInfo.ScriptName
+                    Line      = $e.InvocationInfo.ScriptLineNumber
+                    Column    = $e.InvocationInfo.OffsetInLine
+
                 }
-                
+
                 # output information. Post-process collected info, and log info (optional)
-                $info
-                
+                Write-Output -InputObject $info
+
             }
-            
+
         }
-        
+
     }
-	
+
 	End { }
-	
+
 }
